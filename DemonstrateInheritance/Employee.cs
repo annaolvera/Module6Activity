25695be0d9539d0210772708cd0f10f43dc5feb1
Anna Olvera <azo0049@auburn.edu>
2022-02-15 19:06:06 -0600

    Add project files.

---
+++ DemonstrateInheritance/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemonstrateInheritance
+{
+    public class Employee
+    {
+        //properties
+        public string EmployeeName { get; set; }
+        public int EmployeeNumber { get; set; }
+
+    }
+}
---
+++ DemonstrateInheritance/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace DemonstrateInheritance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblEmployeeNumber = new System.Windows.Forms.Label();
+            this.lblShift = new System.Windows.Forms.Label();
+            this.lblRate = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(72, 83);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(115, 16);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Employee Name: ";
+            // 
+            // lblEmployeeNumber
+            // 
+            this.lblEmployeeNumber.AutoSize = true;
+            this.lblEmployeeNumber.Location = new System.Drawing.Point(61, 140);
+            this.lblEmployeeNumber.Name = "lblEmployeeNumber";
+            this.lblEmployeeNumber.Size = new System.Drawing.Size(126, 16);
+            this.lblEmployeeNumber.TabIndex = 1;
+            this.lblEmployeeNumber.Text = "Employee Number: ";
+            // 
+            // lblShift
+            // 
+            this.lblShift.AutoSize = true;
+            this.lblShift.Location = new System.Drawing.Point(56, 191);
+            this.lblShift.Name = "lblShift";
+            this.lblShift.Size = new System.Drawing.Size(131, 16);
+            this.lblShift.TabIndex = 2;
+            this.lblShift.Text = "Shift Day -1 , Night -2:";
+            // 
+            // lblRate
+            // 
+            this.lblRate.AutoSize = true;
+            this.lblRate.Location = new System.Drawing.Point(79, 241);
+            this.lblRate.Name = "lblRate";
+            this.lblRate.Size = new System.Drawing.Size(108, 16);
+            this.lblRate.TabIndex = 3;
+            this.lblRate.Text = "Hourly Pay Rate:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(206, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(206, 140);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(206, 191);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(173, 22);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(206, 241);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(173, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 475);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblRate);
+            this.Controls.Add(this.lblShift);
+            this.Controls.Add(this.lblEmployeeNumber);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblEmployeeNumber;
+        private System.Windows.Forms.Label lblShift;
+        private System.Windows.Forms.Label lblRate;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
